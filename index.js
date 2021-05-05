794b4d2f1cc357a59c9b6b83368b7bd0711e43cf
Mdkhaki <mdkhaki@yahoo.com>
2021-05-05 13:13:51 +0430

    mongodb setup

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD,MONGO_IP,MONGO_PORT } = require("./config/config");
 
 const app = express();
 
-app.get("/" , (req, res) => {
-    res.send("<h2> Hi there!sdf!</h2>")
-})
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+mongoose
+  .connect(mongoURL, {
+      useNewUrlParser:true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+  })
+  .then(() => console.log("Connected successfully!"))
+  .catch((e) => console.log(e));
+
+app.get("/", (req, res) => {
+  res.send("<h2> Hi there!</h2>");
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log('listening on port :' + port));
+app.listen(port, () => console.log("listening on port :" + port));
